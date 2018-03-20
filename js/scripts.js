a88d00d524b55d56d8bd007e0f391f63cf40e363
Azamat Bekmuratov and Jimmy McNamara <jimmy@>
2018-03-20 11:27:54 -0700

    add js and jquery files and make webpage dynamic by highlighting paragraphs after clicking on them

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("p#highlight1").click(function() {
+    $("p").removeClass();
+    $("p#highlight1").addClass("highlight");
+  });
+
+  $("p#highlight2").click(function() {
+    $("p").removeClass();
+    $("p#highlight2").addClass("highlight");
+  });
+
+  $("p#highlight3").click(function() {
+    $("p").removeClass();
+    $("p#highlight3").addClass("highlight");
+  });
+
+  $("p#highlight4").click(function() {
+    $("p").removeClass();
+    $("p#highlight4").addClass("highlight");
+  });
+
+});
